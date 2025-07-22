47faae48b43c36ed18729934f4fec2d0454abd18
Pnilay <pnilay926@gmail.com>
2025-07-22 18:19:37 -0400

    Components and Pages for the Website

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Navbar />
+      <h1>This is Home page</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between mx-0 my-0 bg-amber-300 px-12 h-[70px] font-semibold">
+      <div className="flex flex-col justify-center grow">
+        <img src="/KitchenQuest_Logo.svg" alt="..." className="w-32" />
+      </div>
+      <div className="flex flex-col justify-center grow-2 mx-24">
+        <ul className="flex flex-row justify-center">
+          <li className="px-6">Home</li>
+          <li className="px-6">Recipes</li>
+          <li className="px-6">Meal Plan</li>
+          <li className="px-6">About Us</li>
+        </ul>
+      </div>
+      <div className="flex justify-end items-center grow">
+        <button className="text-white bg-green-500 rounded-md hover:bg-green-400 sm:w-auto sm:mb-0 px-5 py-2">Sign In</button>
+        <button className="text-white bg-green-500 rounded-md hover:bg-green-400 sm:w-auto sm:mb-0 ml-4 px-5 py-2">Sign Up</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

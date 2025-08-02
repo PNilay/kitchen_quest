a7e86cc9a0af3ff1ee572f7a1013d87d77b5789b
Pnilay <pnilay926@gmail.com>
2025-08-02 19:43:31 -0400

    account page link updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,10 +16,10 @@ function Navbar() {
       </div>
       <div className="flex justify-end items-center grow">
         <div className="text-white bg-green-500 rounded-md hover:bg-green-400 sm:w-auto sm:mb-0 px-5 py-2">
-          <a href="/Account">Sign In</a>
+          <a href="/account">Sign In</a>
         </div>
         <div className="text-white bg-green-500 rounded-md hover:bg-green-400 sm:w-auto sm:mb-0 ml-4 px-5 py-2">
-          <a href="/Account">Sign Up</a>
+          <a href="/account">Sign Up</a>
         </div>
       </div>
     </nav>

42da387f955ab3144f7a3a08b39a9071937b9ef2
Pnilay <pnilay926@gmail.com>
2025-08-05 20:37:14 -0400

    sign in page

---
+++ src/app/Account/signin/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default page;

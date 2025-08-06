f11a1c25ce5f76b57da9204f5a8354c4b66d3c18
Pnilay <pnilay926@gmail.com>
2025-08-05 21:45:37 -0400

    adding account folder

---
+++ src/app/account/register/page.tsx
@@ -0,0 +1,14 @@
+import SignupForm from "@/components/SignupForm";
+import { motion } from "motion/react";
+import Link from "next/link";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default page;
